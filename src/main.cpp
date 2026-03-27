a8de9894a040f805890eaf29ed83163a7694dc9c
Wojciech Adrych <26417549+wadrych@users.noreply.github.com>
2026-03-27 19:12:02 +0100

    initial cmake project structure

---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+  std::cout << "Hello, C-FIO" << std::endl;
+  return 0;
+}
---
+++ tests/test_smoke.cpp
@@ -0,0 +1,3 @@
+#include <gtest/gtest.h>
+
+TEST(Smoke, Passes) { EXPECT_TRUE(true); }
